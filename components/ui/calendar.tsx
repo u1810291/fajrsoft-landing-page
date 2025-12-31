cd3328c80fdc40fdcfe5e90233b2ed0c7bd12fa5
u1810291 <otabekbutcher@gmail.com>
2025-12-31 18:33:18 +0400

    fix: fixed deployement issue in vercel

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
 
 import { cn } from "./utils";
 import { buttonVariants } from "./button";
